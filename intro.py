9d4e60c6993c1bc85a733eb20689a7f56c715659
Andrii Tsurkan <andtsu@kvn.no>
2026-09-02 10:35:16 +0200

    start på gjettespill

---
+++ intro.py
@@ -0,0 +1,15 @@
+import random
+
+tall = random.randint(0, 1000)
+
+for x in range(10):
+    svar = int(input("Gjett tallet: "))
+
+    if svar == tall:
+        print("Riktig! Du fant tallet!")
+        break
+    else:
+        print("Du har", 9 - x, "forsøk igjen.")
+
+else:
+    print("Du har brukt opp alle forsøkene. Du tapte!")
